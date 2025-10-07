2e57fb1a4d6de9fe313bd3785c702c23b75b9deb
LivinkumarS <001liv100@ggmail.com>
2025-10-07 20:59:09 +0530

    mic update

--- frontend/src/components/speechToText.jsx
+++ frontend/src/components/speechToText.jsx
@@ -58,6 +58,7 @@ export default function SpeechToText({ handleSubmit, setScript }) {
       }
     } else {
       SpeechRecognition.stopListening();
+      resetTranscript();
       handleSubmit();
     }
   };
